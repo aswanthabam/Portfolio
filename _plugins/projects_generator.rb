8b225d45e738f88793aebdbf9669f8196d6a8af8
Aswanth Vc <aswanth.abam@gmail.com>
2024-05-10 22:04:10 +0530

    fix : Updated firebase.json, feat : Project links changed

--- _plugins/projects_generator.rb
+++ _plugins/projects_generator.rb
@@ -5,9 +5,10 @@ require 'date'
 data = YAML.safe_load(File.read('_data/projects.yml'), [Date])
 
 data.each do |entry|
-    title = entry['name']
+    id = entry['id']
+    title = entry['name'] + " - Project"
     description = entry['description']
-    File.open("project/#{title.downcase.gsub(' ', '-')}.html", "w") do |file|
+    File.open("project/#{id}.html", "w") do |file|
         file.write("---\nlayout: project\n")
         file.write("title: #{title}\n")
         file.write("entry: #{description}\n")
